5a2e49226dd26e5ffc26f4efb662aadfbdbd6f73
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-04 14:50:02 +0500

    search in api done

--- index.js
+++ index.js
@@ -1,39 +1,24 @@
 const express = require('express');
+const product = require('./product');
 require("./config");
 const Product = require('./product');
 const app = express();
 
 app.use(express.json());
 
-// inserting data to database with api
-app.post("/create", async (req, resp) => {
-    let data = new Product(req.body);
-    const result = await data.save();
-    resp.send(result);
-    console.log(result);
-});
-
-// getting data or reading data from the databse with api
-app.get("/list", async (req, resp) => {
-    let data = await Product.find();
-    resp.send(data);
+// search the data with api ...
+app.get('/search/:key', async (req,resp)=>{
+   console.log(req.params.key);
+   let data = await Product.find(
+      {
+         "$or":[
+            { "name":{$regex:req.params.key}},
+            { "brand":{$regex:req.params.key}},
+         ]
+      }
+   );
+   resp.send(data);
 })
 
-// deleting data with api
-app.delete("/delete/:_id", async (req, resp) => {
-    console.log(req.params)
-    let data = await Product.deleteOne(req.params);
-    resp.send(data);
-})
-
-//  updating data with api in the database
-app.put("/update/:_id",async (req, resp) => {
-    console.log(req.params)
-    let data = await Product.updateOne(
-        req.params,
-        {$set: req.body}
-    );
-    resp.send(data);
-})
 
 app.listen(5000)
