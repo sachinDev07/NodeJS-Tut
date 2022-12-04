d5ae635cefc54e81d6fa7612537674253b31b2ba
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-04 23:21:06 +0500

    post api-for inserting data

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ const express = require('express');
 const con =require('./config');
 const app = express();
 
+app.use(express.json());
+
 // get api 
 app.get('/', (req,resp)=>{
    con.query("select * from student",(err, result) => {
       if(err){
          resp.send("error")
+      }else {
+         resp.send(result)
+      }
+   })
+});
+
+// post api...inserting data
+app.post('/', (req,resp)=>{
+   const data = req.body;
+   con.query('INSERT INTO student SET ?', data, (err, result, fields)=> {
+      if(err){
+         resp.send('error')
       }else {
          resp.send(result)
       }
