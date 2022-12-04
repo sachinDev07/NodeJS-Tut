8786e3d92a70010a7f6672e08521f4373678b050
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-04 14:13:12 +0500

    created schema for the product

---
+++ product.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const productSchema= mongoose.Schema({
+    name:String,
+    brand:String,
+    price:Number
+});
+
+module.exports= mongoose.model("products",productSchema);
